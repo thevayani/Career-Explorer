b462fdf7988d18bf89150db866b0017cc732a078
thevayani <ithevayani1601@gmail.com>
2025-04-17 10:45:08 +0530

    project

---
+++ src/main.jsx
@@ -0,0 +1,76 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx' 
+import store from './redux/store.js'
+import { Provider } from 'react-redux'
+import Show from './pages/quiz/Show.jsx'
+import QuestionAi from './pages/quiz/QuestionAi.jsx'
+import Selfintro from './pages/Selfintro.jsx'
+import Resume from './pages/Resume.jsx'
+import UserDetails from './pages/UserDetails.jsx'
+import ShowUser from './pages/ShowUser.jsx'
+import CareerGoals from './pages/CareerGoals.jsx'
+import CareerAi from './pages/CareerAi.jsx'
+import Header from './pages/Header.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: (<Register/>),
+  },
+
+ {
+    path: "/login",
+    element: (<Login/>),
+  },
+  {
+    path: "/details",
+    element: (<UserDetails/>),
+  },
+  {
+    path: "/goal",
+    element: (<CareerGoals/>),
+  },
+
+  {
+    path: "/show",
+    element: (<ShowUser/>),
+  },
+
+  {
+    path: "/careerAi",
+    element: (<CareerAi/>),
+  },
+  {
+    path: "/header",
+    element: (<Header/>),
+  },
+  {
+    path: "/resume",
+    element: (<Resume/>),
+  },
+  {
+    path: "/intro",
+    element: (<Selfintro/>),
+  },
+  {
+    path: "/quiz",
+    element: (<QuestionAi/>),
+  },
+  {
+    path: "/quizshow",
+    element: (<Show/>),
+  },
+  
+]);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+  <RouterProvider router={router} />
+  </Provider>
+);
---
+++ src/pages/Selfintro.jsx
@@ -0,0 +1,88 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+function Selfintro() {
+  let user = JSON.parse(localStorage.getItem("users"));
+  const cx = "84c171dacf1aa43c1"
+  const apiKey = "AIzaSyC8kh_wDAmTboxQf3lvjBSChxhiNfjbPdU"
+
+  const [summary, setSummary] = useState("")
+  const [userInputValue, setuserInputValue] = useState(null);
+  
+
+  useEffect(() => {
+    getApi()
+  }, [])
+
+ 
+
+  const getApi = () => {
+
+    axios.get(`https://thevayani.pythonanywhere.com/profile/${user.id}`).then((res) => {
+      let getData = res.data.data.data
+      console.log(getData)
+      setuserInputValue(JSON.parse(getData))
+
+    });
+  }
+
+
+  const genAI = new GoogleGenerativeAI(apiKey);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+
+  useEffect(() => {
+    run()
+  }, [])
+
+  async function run() {
+    const prompt = `Hai, based on the  details how to introduce elaborate selfintroduction:
+        ${JSON.stringify(userInputValue)}
+        avoid text outside HTML tags return only the HTML content`;
+
+
+
+
+
+
+    const result = await model.generateContent(prompt);
+    console.log("Summary Response:", result.response.text());
+
+
+    const responseText = result.response.text();
+
+    setSummary(responseText);
+    console.log(summary)
+  }
+
+
+
+  return <div>
+    <h2 style={{ textAlign: "center", color: "black" }}>Self Introduction</h2>
+
+    <button varient="primary" style={{ textAlign: "center", color: "black" }} onClick={run} >Generate Introduction</button>
+
+    <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+  </div>
+
+
+
+
+}
+export default Selfintro
